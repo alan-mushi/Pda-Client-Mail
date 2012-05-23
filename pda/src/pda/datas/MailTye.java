2d20f29b165f27bd81fc1ce77fe32cf158400e3a
zipang29 <guillaume.claudic@laposte.net>
2012-05-23 16:23:41 +0200

    Ajout de la classe MailType

---
+++ pda/src/pda/datas/MailTye.java
@@ -0,0 +1,17 @@
+package datas;
+
+public class MailType extends MailContent {
+	private String type;
+	
+	public MailType() {
+		super();
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String unType) {
+		this.type = unType;
+	}
+}
