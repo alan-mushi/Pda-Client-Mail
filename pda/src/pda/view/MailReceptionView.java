3784be2cbdeb1df16e8c52082e5465bfefcc5b48
zipang29 <guillaume.claudic@laposte.net>
2012-06-09 16:56:36 +0200

    Amélioration de l'interface de la boite de réception

--- pda/src/pda/view/MailReceptionView.java
+++ pda/src/pda/view/MailReceptionView.java
@@ -20,21 +20,39 @@ public class MailReceptionView {
 		initialiserGui();
 	}
 	public void initialiserGui() {
-		mainPanel.setLayout(new GridLayout(2, 1));
+		mainPanel.setLayout(new BorderLayout());
 	
 		nouveau = new JButton("Nouveau");
 		supprimer = new JButton("Supprimer");
 		
 		listeMail = new DefaultListModel();
-		listeMail.addElement("Mail1");
+		JLabel mail1 = new JLabel("Mail1", new ImageIcon("nonLu.png"), SwingConstants.LEFT);
+		listeMail.addElement(mail1);
 		listeMail.addElement("Mail2");
 		listeMail.addElement("Mail3");
+		listeMail.addElement("Mail4");
+		listeMail.addElement("Mail5");
+		listeMail.addElement("Mail6");
+		listeMail.addElement("Mail7");
+		listeMail.addElement("Mail8");
+		listeMail.addElement("Mail9");
+		listeMail.addElement("Mail10");
+		listeMail.addElement("Mail11");
+		listeMail.addElement("Mail12");
+		listeMail.addElement("Mail13");
+		listeMail.addElement("Mail14");
+		listeMail.addElement("Mail15");
+		listeMail.addElement("Mail16");
+		listeMail.addElement("Mail17");
+		listeMail.addElement("Mail18");
 		listeMailGUI = new JList(listeMail);
+		listeMailGUI.setFixedCellHeight(50);
+		JScrollPane listeScroll = new JScrollPane(listeMailGUI);
 		
-		mainPanel.add(listeMailGUI);
+		mainPanel.add(listeScroll, BorderLayout.CENTER);
 		JPanel panelBas = new JPanel(new GridLayout(1, 2));
 		panelBas.add(nouveau);
 		panelBas.add(supprimer);
-		mainPanel.add(panelBas);
+		mainPanel.add(panelBas, BorderLayout.SOUTH);
 	}
 }
