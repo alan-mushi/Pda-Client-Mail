2bfd0e010a1e67253f262786408ff54f4cfe2e6d
Thibault GUITTET <alan_mushi@yahoo.com>
2012-06-10 18:31:06 +0200

    MailType remplie.

--- pda/src/pda/datas/MailType.java
+++ pda/src/pda/datas/MailType.java
@@ -1,19 +1,59 @@
-package pda.datas;
+package pda.datas ;
 
 import pdaNetwork.misc.MailContent ;
 
-public class MailType extends MailContent {
+/**
+ * Cette classe est une surchouche de la classe MailContent. Elle a l'atout 
+ * d'implémenter Serializable et StaticRefs.
+ * @see pdaNetwork.misc.MailContent
+ */
+public class MailType extends MailContent implements java.io.Serializable , StaticRefs {
+	/** Le type du message. */
 	private String type;
+	private static final long serialVersionUID = 3L ;
+	public static final String RECU = "recu" , ENVOYE = "envoye" , BROUILLON = "brouillon" , LU = "lu" ;
 	
-	public MailType() {
-		super( "" , "" , "");
+	/**
+	 *
+	 */
+	public MailType( String recipient , String object , String text , String tmpType ) throws IllegalArgumentException {
+		super( recipient , object , text , user , null ) ;
+		if ( tmpType == null || tmpType.isEmpty() ) {
+			throw new IllegalArgumentException( "Le type du message n'est pas défini." ) ;
+		}
+		else if ( tmpType.equals( RECU ) || tmpType.equals( ENVOYE ) || tmpType.equals( BROUILLON ) || tmpType.equals( LU ) ) {
+			this.type = tmpType ; 
+		}
+		else { 
+			throw new IllegalArgumentException( "Le type du message n'est pas correct." ) ;
+		}
 	}
 	
+	/**
+	 *
+	 */
 	public String getType() {
 		return type;
 	}
 	
+	/**
+	 *
+	 */
 	public void setType(String unType) {
 		this.type = unType;
+	}
+
+	/**
+	 *
+	 */
+	public String toString() {
+		String res = "type : " + this.getType() ;
+		res += "\nexpéditeur : " + this.getExpeditor() ;
+		res += "\nobjet : " + this.getObject() ;
+		res += "\ndestinataire : " + this.getRecipient() ;
+		res += "\ndate création : " + this.getDate() ;
+		res += "\n------------------------------" ;
+		res += "\nContenu du message : \n" + this.getText() ;
+		return ( res ) ;
 	}
 }
--- pda/src/pda/datas/StaticRefs.java
+++ pda/src/pda/datas/StaticRefs.java
@@ -7,6 +7,10 @@ public interface StaticRefs {
 	
 	/** Référence sur l'objet DB. */
 	public static final DB myDB = new DB() ;
+	/** Nom de fichier pour l'objet Login. */
+	public static final String loginFile = "login.bin" ;
 	/** Nom de fichier pour l'objet Contacts. */
 	public static final String contactsFile = "contacts.bin" ;
+	/** Nom de l'utilisateur. */
+	public static final String user = "Not set yet!" ;
 }
