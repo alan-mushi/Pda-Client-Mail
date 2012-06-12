d2caa57e5d0d60f9da578eee2f8ad37cd82ac9d5
Thibault GUITTET <alan_mushi@yahoo.com>
2012-06-12 20:23:24 +0200

    Ajout de la classe de synchronisation.

---
+++ pda/src/pda/datas/Sync.java
@@ -0,0 +1,73 @@
+package pda.datas ;
+
+import pdaNetwork.client.service.MailClient ;
+import pdaNetwork.misc.ProtocolException ;
+import java.util.ArrayList ;
+
+/**
+ * Syncronise la liste des messages en local avec le serveur.
+ */
+public class Sync implements StaticRefs {
+
+	/** Indique si la dernière connection a pu être établie. */
+	private boolean lastConnectionSucced ;
+	/** Référence privée sur Mail. */
+	private Mail myMail ;
+	/** Références privées pour les logins du serveur. */
+	private String user, passwd ;
+
+	/**
+	 * Ce constructeur effectue la synchronisation des messages.
+	 */
+	public Sync( Mail MailObject ) {
+		try {
+			this.myMail = MailObject ;
+			this.deleteOnServer() ;
+			this.getNewMails() ;
+			this.lastConnectionSucced = true ;
+		} catch ( ProtocolException e ) {
+			this.lastConnectionSucced = false ;
+		}
+	}
+
+	/**
+	 * Supprime tous les messages du serveur supprimés en local.
+	 */
+	private void deleteOnServer() {
+		try {
+			ArrayList<String> toDel = this.myMail.getSupprList() ;
+			MailClient deleter = new MailClient( this.user , this.passwd ) ;
+			for ( int i = 0 ; i < toDel.size() ; i++ ) {
+				deleter.delete( toDel.get(i) ) ;
+			}
+			deleter.close() ;
+		} catch ( ProtocolException e ) {
+			this.lastConnectionSucced = false ;
+		}
+	}
+
+	/**
+	 * Ajoute les nouveaux mails en provenance du serveur à Mail.
+	 */
+	private void getNewMails() {
+		try {
+			MailClient receiver = new MailClient( this.user , this.passwd ) ;
+			ArrayList<String> newMails = receiver.getHeaders() ;
+			for ( int i = 0 ; i < newMails.size() ; i++ ) {
+				String id = newMails.get(i) ;
+				MailContent email = receiver.receive( id ) ;
+				this.myMail.add( id , email , MailType.RECU ) ;
+			}
+			receiver.close() ;
+		} catch ( ProtocolException e ) {
+			this.lastConnectionSucced = false ;
+		}
+	}
+		
+	/**
+	 * Retourne si la dernière connection au serveur a été possible.
+	 */
+	public boolean getLastConnectionSucced() {
+		return ( this.lastConnectionSucced ) ;
+	}
+}
