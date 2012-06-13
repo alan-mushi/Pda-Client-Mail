7efbde9d98d20d2fbde3b8132b798fd65be8c097
zipang29 <guillaume.claudic@laposte.net>
2012-06-13 14:09:56 +0200

    Documentation de MailParamCtrl, MailSelectContactCtrl, MailSupprCtrl, ModeleTableau et fin de la doc pour les controleurs

--- pda/src/pda/control/MailSelectContactCtrl.java
+++ pda/src/pda/control/MailSelectContactCtrl.java
@@ -5,13 +5,26 @@ import pda.datas.*;
 import javax.swing.*;
 import java.awt.event.*;
 
+/**
+* Classe gérant les actions pour l'interface de sélection des contacts avant l'envoi d'un mail
+*/
 public class MailSelectContactCtrl implements ActionListener {
 	
+	/** Une référence vers la vue*/
 	private MailSelectContactView view;
 	
+	/**
+	* Constructeur
+	* @param view Une référence vers la vue correspondante au controleur.
+	*/
 	public MailSelectContactCtrl(MailSelectContactView view) {
 		this.view = view;
 	}
+	
+	/**
+	* Gère les évènements pour l'interface lors de la sélection d'un contact avant l'envoie d'un mail.
+	* @param e L'évènement.
+	*/
 	public void actionPerformed(ActionEvent e) {
 		Object src = e.getSource();
 		if(src == this.view.getBoutonRetour()) {
--- pda/src/pda/datas/ModeleTableau.java
+++ pda/src/pda/datas/ModeleTableau.java
@@ -2,34 +2,70 @@ package pda.datas;
 
 import javax.swing.table.AbstractTableModel;
 
+/**
+* Classe gérant un modèle de tableau personnalisé pour l'affichage dans un tableau de la liste des mails.
+*/
 public class ModeleTableau extends AbstractTableModel {
-
+	
+	/** Les données du tableau */
 	private Object[][] data;
+	
+	/** Les entêtes de colones */
 	private String[] title;
 	
+	/** Le serialVersionUID pour identifier la classe */
 	private static final long serialVersionUID = 30L;
 	
+	/**
+	* Constructeur
+	* @param data Les données du tableau.
+	* @param Les entêtes des colones du tableau.
+	*/
 	public ModeleTableau(Object[][] data, String[] title) {
 		this.data = data;
 		this.title = title;
 	}
 	
+	/**
+	* Retourne le nombre de colones du tableau.
+	* @return Le nombre de colones du tableau.
+	*/
 	public int getColumnCount() {
 		return this.title.length;
 	}
 	
+	/**
+	* Retourne le nombre de ligne du tableau
+	* @return Le nombre de ligne dans le tableau
+	*/
 	public int getRowCount() {
 		return this.data.length;
 	}
 	
+	/**
+	* Retourne la valeur d'une case du tableau.
+	* @param row Le numéro de ligne.
+	* @param col Le numéro de colone.
+	* @return Le contenu de la case demandée via les paramètres.
+	*/
 	public Object getValueAt(int row, int col) {
 		return this.data[row][col];
 	}
 	
+	/**
+	* Retourne le nom d'une colone.
+	* @param col Le numéro de la colones.
+	* @return Le nom de la colone.
+	*/
 	public String getColumnName(int col) {
  		return this.title[col];
 	}
 	
+	/**
+	* Retourne le type de Class de la colone.
+	* @param col Le numéro de colones.
+	* @return Le nom de la classe de la colone.
+	*/
 	public Class getColumnClass(int col){
 		return this.data[0][col].getClass();
 	}
