467976c9e2380e49dbc40201e63497454e0b8bc1
Thibault GUITTET <alan_mushi@yahoo.com>
2012-06-16 14:21:36 +0200

    Changement de location des fichiers .bin

--- pda/src/pda/datas/StaticRefs.java
+++ pda/src/pda/datas/StaticRefs.java
@@ -7,10 +7,28 @@ public interface StaticRefs {
 	
 	/** Référence sur l'objet DB. */
 	public static final DB myDB = new DB() ;
-	/** Nom de fichier pour l'objet Login. */
-	public static final String loginFile = "login.bin" ;
-	/** Nom de fichier pour l'objet Contact. */
-	public static final String contactsFile = "contacts.bin" ;
-	/** Nom de fichier pour l'objet Mail. */
-	public static final String mailsFile = "mails.bin" ;
+
+	/** Dossier principal de stockage. */
+	public static final String root = "data/" ;
+	/** Nom du fichier pour l'objet Login. */
+	public static final String loginFile = root + "login.bin" ;
+	/** Nom du fichier pour l'objet Contact. */
+	public static final String contactsFile = root + "contacts.bin" ;
+	/** Nom du fichier pour l'objet Mail. */
+	public static final String mailsFile = root + "mails.bin" ;
+
+	/** Dossier de stockage des HashMaps. */
+	public static final String content = root + ".mapContent/" ;
+	/** Nom du fichier de sauvegarde de la HashMap recus. */
+	public static final String mapRecus = content + "recus.bin" ;
+	/** Nom du fichier de sauvegarde de la HashMap lus. */
+	public static final String mapLus = content + "lus.bin" ;
+	/** Nom du fichier de sauvegarde de la HashMap envoyes. */
+	public static final String mapEnvoyes = content + "envoyes.bin" ;
+	/** Nom du fichier de sauvegarde de la HashMap brouillons. */
+	public static final String mapBrouillons = content + "brouillons.bin" ;
+	/** Nom du fichier de sauvegarde de la HashMap toSend. */
+	public static final String mapToSend = content + "toSend.bin" ;
+	/** Nom du fichier de sauvegarde de l'ArrayList Supprimes. */
+	public static final String lsitSuppr = content + "suppr.bin" ;
 }
